7ddb9c742183b1023c20e9e6c6efe96e4cfb1396
DanielVataga <danikvataga77@gmail.com>
2025-08-13 13:28:04 +0100

    add env var for WebSocket server

--- client/client-app/src/App.tsx
+++ client/client-app/src/App.tsx
@@ -55,7 +55,7 @@ export default function App() {
   const [connected, setConnected] = useState(false);
 
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:8080");
+    const ws = new WebSocket(process.env.REACT_APP_WS_URL || "ws://localhost:8080");
 
     ws.onopen = () => {
       setConnected(true);
